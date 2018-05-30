87c64ae997f34766347dd41ad10d400940b80563
Andre Mare <andre.mare@gmail.com>
2018-05-30 19:00:13 +0200

    Add InsertionSortRecursive Algorithm

---
+++ sort/src/test/java/com/code2bits/algorithm/sort/InsertionSortRecursiveTest.java
@@ -0,0 +1,59 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2017 André Maré
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.code2bits.algorithm.sort;
+
+
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
+
+/**
+ * The InsertionSortRecursiveTest class forms part of the Test Driven Development approach to test the 
+ * InsertionSort class.
+ * 
+ * @author	André Maré
+ */
+public class InsertionSortRecursiveTest {
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInsertionSortRecursiveNull() {
+		int[] unsortedArray = null;
+		new InsertionSortRecursive().sort(unsortedArray);
+	} 
+ 
+	@Test
+	public void testInsertionSortRecursive() {
+		int input[] = new int[]{6,1,7,9,3,8,2,5,4,0};
+		int sorted[] = {0,1,2,3,4,5,6,7,8,9};
+		InsertionSortRecursive sort = new InsertionSortRecursive();
+		sort.sort(input);
+
+		assertEquals(true, Arrays.equals(input, sorted));
+	}
+	
+}
