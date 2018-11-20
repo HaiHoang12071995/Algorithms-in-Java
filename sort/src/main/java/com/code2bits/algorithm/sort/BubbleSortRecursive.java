c26cfaa5409ce94809c0cc5c19cef8857bcb152f
Andre Mare <andre.mare@gmail.com>
2018-11-20 07:13:25 +0200

    Clean Code: Remove JavaDocs from methods.

--- sort/src/main/java/com/code2bits/algorithm/sort/BubbleSortRecursive.java
+++ sort/src/main/java/com/code2bits/algorithm/sort/BubbleSortRecursive.java
@@ -25,11 +25,10 @@ package com.code2bits.algorithm.sort;
 
 
 /**
- * The BubbleSort class implements the BubbleSort algorithm for sorting an array of integers. 
- *  
- * The Bubble Sort algorithm repeatedly steps through the list and compare each adjacent item. The pair 
- * of values gets swapped if they are in the wrong order. The algorithm gets its name from the way smaller 
- * or larger elements "bubble" to the top of the list.
+ * The BubbleSortRecursive class implements the BubbleSort algorithm for sorting an array of integers. The 
+ * Bubble Sort algorithm repeatedly steps through the list and compare each adjacent item. The pair of values 
+ * gets swapped if they are in the wrong order. The algorithm gets its name from the way smaller or larger 
+ * elements "bubble" to the top of the list.
  * 
  * The BubbleSortRecursive implements the BubbleSort algorithm by making use of recursion.
  * 
@@ -38,47 +37,35 @@ package com.code2bits.algorithm.sort;
 public final class BubbleSortRecursive {
 
 		
-	/**
-	 * The sort method is invoked by external classes to sort an array of integers by making use of the 
-	 * BubbleSort algorithm.
-	 */
-	public void sort(int[] collection) {
-		if (collection != null) {
-			bubbleSort(collection, collection.length);
-		} else {
-			throw new IllegalArgumentException("Input parameter for array to sort is null.");
-		}
-	} 
+    public void sort(int[] collection) {
+        if (collection != null) {
+            bubbleSort(collection, collection.length);
+        } else {
+            throw new IllegalArgumentException("Input parameter for array to sort is null.");
+        }
+    } 
 	
 	
-	/**
-	 * The Bubble Sort algorithm repeatedly steps through the list and compare each adjacent item. The pair 
-	 * of values gets swapped if they are in the wrong order. The method implements it through one pass of 
-	 * the array, whereby the largest element is moved to the last (top) position of the array. The process 
-	 * is repeated by recursively invoking the method excluding the last (top) position of the array.
-	 */
-	private void bubbleSort(int[] collection, int arrayLength) {
-		if (arrayLength == 1) {
-			return;
-		}
+    private void bubbleSort(int[] collection, int arrayLength) {
+        if (arrayLength == 1) {
+            return;
+        }
 		
-		for (int i=0; i < arrayLength-1; i++) {
-			if (collection[i] > collection[i+1]) {
-				swap(collection, i, i+1);
-			}
-		}
+        for (int i=0; i < arrayLength-1; i++) {
+            if (collection[i] > collection[i+1]) {
+                swap(collection, i, i+1);
+            }
+        }
 		
-		bubbleSort(collection, arrayLength-1);
-	}
+        bubbleSort(collection, arrayLength-1);
+    }
         
-        
-	/**
-	 * The method swaps two values around within an array based on the two input parameters x and y.
-	 */
-	private void swap(int[] collection, int x, int y) {
-		int temp = collection[x];
-		collection[x] = collection[y];
-		collection[y] = temp;
-	}
+    
+    private void swap(int[] collection, int x, int y) {
+        int temp = collection[x];
+        collection[x] = collection[y];
+        collection[y] = temp;
+    }
 	
+    
 }
