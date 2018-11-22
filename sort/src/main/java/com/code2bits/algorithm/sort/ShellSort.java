8e5a92e6d18a97d1e6ee00e9720485a9c9530c38
Andre Mare <andre.mare@gmail.com>
2018-11-22 07:49:03 +0200

    Clean Code: Remove JavaDocs from methods.

--- sort/src/main/java/com/code2bits/algorithm/sort/ShellSort.java
+++ sort/src/main/java/com/code2bits/algorithm/sort/ShellSort.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  * 
- * Copyright (c) 2017 André Maré
+ * Copyright (c) 2018 André Maré
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
