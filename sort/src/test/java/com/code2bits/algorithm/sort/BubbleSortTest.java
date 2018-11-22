acadec0ed4465d84231efb4a10e8599f672834a5
Andre Mare <andre.mare@gmail.com>
2018-11-22 07:48:43 +0200

    Clean Code: Remove JavaDocs from methods.

--- sort/src/test/java/com/code2bits/algorithm/sort/BubbleSortTest.java
+++ sort/src/test/java/com/code2bits/algorithm/sort/BubbleSortTest.java
@@ -32,28 +32,28 @@ import org.junit.Test;
 
 
 /**
- * The BubbleSortTest class forms part of the Test Driven Development approach to test the 
- * BubbleSort class.
+ * The BubbleSortTest class forms part of the Test Driven Development approach to test the BubbleSort class.
  * 
  * @author	André Maré
  */
 public class BubbleSortTest {
 
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void testBubbleSortNull() {
-		int[] unsortedArray = null;
-		new BubbleSort().sort(unsortedArray);
-	} 
+    @Test(expected = IllegalArgumentException.class)
+    public void testBubbleSortNull() {
+        int[] unsortedArray = null;
+        new BubbleSort().sort(unsortedArray);
+    } 
  
-	@Test
-	public void testBubbleSort() {
-		int input[] = new int[]{6,1,7,9,3,8,2,5,4,0};
-		int sorted[] = {0,1,2,3,4,5,6,7,8,9};
-		BubbleSort sort = new BubbleSort();
-		sort.sort(input);
+    
+    @Test
+    public void testBubbleSort() {
+        int input[] = new int[]{6,1,7,9,3,8,2,5,4,0};
+        int sorted[] = {0,1,2,3,4,5,6,7,8,9};
+        BubbleSort sort = new BubbleSort();
+        sort.sort(input);
 
-		assertEquals(true, Arrays.equals(input, sorted));
-	}
+        assertEquals(true, Arrays.equals(input, sorted));
+    }
 	
 }
